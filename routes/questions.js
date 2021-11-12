53b190eae39682149ef94f0b986bf6001ef945d3
Abhishek Mishra <amishr1@stevens.edu>
2021-11-12 16:21:20 -0500

    get

--- routes/questions.js
+++ routes/questions.js
@@ -1,2 +1,12 @@
 const express = requre('express');
 
+router = express.Router();
+
+router.get('/edit/:id', async(req,res) => {
+
+});
+
+router.put('/:id', async(req, res) => {
+    info = req.body;
+    
+});
