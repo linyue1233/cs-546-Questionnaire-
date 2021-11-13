451b3de7a3a3cd83c84b5911a5968c757d574652
linyue1233 <1191609469@qq.com>
2021-11-13 13:35:00 -0500

    change id'type to uuid

--- data/questions.js
+++ data/questions.js
@@ -1,25 +1,16 @@
 // Add DB operations on questions here.
 const mongoCollections = require('../config/mongoCollections');
 let questions = mongoCollections.questions;
-
+const uuid = require('uuid/v4');
 
 let exportMethods = {
     // get details of an individual question
-    async getQuestionById(questionId){
+    async getQuestionById(id){
         if(arguments.length > 1){
             throw `you can not pass more parameters`;
         }
-        if(questionId == undefined){
-            throw `you must provide an id for search`;
-        }
-        if(typeof id !== 'string'){
-            throw 'Id must be a string';
-        }
-        if( id.trim() === ""){
-            throw 'id is whitespace';
-        }
         const questionCollection = await questions();
-        const questionAns = await questionCollection.findOne({_id: questionId});
+        const questionAns = await questionCollection.findOne({_id: id});
         if(questionAns === null){
             throw `no question with this id`;
         }
@@ -34,7 +25,7 @@ let exportMethods = {
         
         const questionCollections = await questionCollection.find({'communityId': communityId, 'posterId': userId}).toArray();
         return questionCollections;
-    }
+    },
 }
 
 module.exports = exportMethods;
