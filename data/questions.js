5772ac89d1e420a417391e51745452baed9d83d5
AravindhShiva <aravindhshiv2@gmail.com>
2021-11-13 02:26:23 -0500

    added tag based search support

--- data/questions.js
+++ data/questions.js
@@ -6,8 +6,14 @@ const search = async (body) => {
   /* Assuming the body comes like this:
      { keyword: <string> } */
   // TODO: add validation wherever necessary
+  let tokenizedKeywords = body.keyword.split(" ");
   const questionsCollection = await questions();
   const allMatches = await questionsCollection.find({ $text: { $search: body.keyword } }).toArray();
+  const allArrayMatches = await questionsCollection.find({ tags: tokenizedKeywords }).toArray();
+  console.log(tokenizedKeywords, allArrayMatches);
+  if (allArrayMatches.length > 0) {
+    allMatches.push(allArrayMatches);
+  }
   return allMatches;
 };
 
