e75f23932e0da252561b6c00c88e5d9148a270c0
mabhi1 <94099384+mabhi1@users.noreply.github.com>
2021-11-12 19:35:40 -0500

    Create index.js

---
+++ data/index.js
@@ -0,0 +1,9 @@
+const communities = require('./communities');
+const questions = require('./questions');
+const users = requrie('./users');
+
+module.exports = {
+    communities: communities,
+    questions: questions,
+    users: users
+};
