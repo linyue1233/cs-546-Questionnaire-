736c4c5622ddbaa487f1baf418142515f45f0280
AravindhShiva <aravindhshiv2@gmail.com>
2021-11-13 01:53:37 -0500

    added indexes for search and built search feature

--- data/questions.js
+++ data/questions.js
@@ -1,1 +1,29 @@
 // Add DB operations on questions here.
+const mongoCollections = require("../config/mongoCollections");
+const questions = mongoCollections.questions;
+
+const search = async (body) => {
+  /* Assuming the body comes like this:
+     { keyword: <string> } */
+  // TODO: add validation wherever necessary
+  const questionsCollection = await questions();
+  const allMatches = await questionsCollection.find({ $text: { $search: body.keyword } }).toArray();
+  return allMatches;
+};
+
+const remove = async (id) => {
+  // return the following object for deletion status: { deleted: true, id: id }
+  // TODO: add validation wherever necessary
+  const questionsCollection = await questions();
+  const removedInfo = await questionsCollection.deleteOne({ _id: id });
+  if (removedInfo.deletedCount === 0) {
+    console.log("Something went wrong during question deletion!");
+    return { deleted: false, id: id };
+  }
+  return { deleted: true, id: id };
+};
+
+module.exports = {
+  search,
+  remove,
+};
