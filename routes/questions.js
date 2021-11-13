65b75689566d50a9bd5d024cea626b1e7f683ac2
Abhishek Mishra <amishr1@stevens.edu>
2021-11-13 02:20:35 -0500

    edit update

--- routes/questions.js
+++ routes/questions.js
@@ -17,6 +17,14 @@ router.get('/edit/:id', async(req,res) => {
 
 router.put('/:id', async(req, res) => {
     info = req.body;
+
+    try{
+        const q = await questions.get(req.params.id);
+        if (!q) throw 'Error: ID not found'
+    }catch(e){
+        res.sendStatus(500);
+    } 
+
     try{
         const updated = await questions.update(req.params.id, info.title, info.description, info.tags, info.communityId);
         if (updated == false){
