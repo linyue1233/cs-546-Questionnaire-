85d153c2d9c9d96052136a837cedd1180a43edcc
Siddharth Godhani <siddharthgodhani@Siddharths-MBP.fios-router.home>
2021-11-27 15:51:55 -0500

    userprofile edit/Added udpdate userpfofile and handlerbar for useredit form

--- data/users.js
+++ data/users.js
@@ -28,7 +28,35 @@ const deleteUser = async (userId) => {
   }
   return { deleted: true, _id: userId };
 };
+const updateUser= async(userId,firstName,lastName,profileImage)=> {
+  const userCollection = await users();
+
+  if(!userId) throw "UserId must be present";
+  if(!firstName) throw "firstName must be present";
+  if(!lastName) throw "lastname must be present";
+  if(!profileImage) throw "profileImage must be present";
+
+
+  let userUpdateInfo = {
+    firstName: firstName,
+    lastName: lastName,
+    profileImage:profileImage
+
+  };
+
+  const updateInfo = await userCollection.updateOne(
+    { _id: userId },
+    { $set: userUpdateInfo }
+  );
+  if (!updateInfo.matchedCount && !updateInfo.modifiedCount)
+    throw 'Update failed';
+
+    const updateduser = await userCollection.findOne({ _id: userId });
+  return updateduser
+  
+}
 
 module.exports = {
   deleteUser,
+  updateUser
 };
--- routes/users.js
+++ routes/users.js
@@ -3,6 +3,22 @@ const router = express.Router();
 const users = require("../data/users");
 const validator = require("../helpers/routeValidators/userValidator");
 
+const path = require("path");
+const multer  = require('multer')
+
+// This will store the image first in temorrarydisk called storage
+let storage= multer.diskStorage({
+  destination:"/public/images/",
+  filename: (req,file,cb)=>{
+    cb(null,file.fieldname+"_"+Date.now()+path.extname(file.originalname));  
+  }
+})
+// defining multer middleware
+
+let upload=multer({
+  storage:storage
+}).single('profileImage');
+// profileImage is the name atribute of html form
 router.delete("/:id", async (req, res) => {
   let userId = req.params.id;
   let validate = validator.validateId(userId);
@@ -20,6 +36,50 @@ router.delete("/:id", async (req, res) => {
       return;
     }
     res.status(400).render("users/get_specific_user", { error: "Something went wrong." });
+    return;
+  } catch (e) {
+    res.status(400).render("users/get_specific_user", { error: e });
+    return;
+  }
+});
+
+
+router.get('/:id/edit', async (req, res) => {
+  let userId = req.params.id;
+  // Kindly verify below function name with data functions
+  const User = await users.getUserbyId(userId);
+
+
+  res.status(200).render('users/Update_userForm',User);
+});
+router.put("/:id", upload, async (req, res, next) => {
+
+ 
+/*   let validate = validator.validateId(userId);
+  if (!validate.isValid) {
+    res.render("users/get_specific_user", { error: validate.message });
+    return;
+  } */
+  try {
+    if (!req.body.firstName|| !req.body.lastName || !req.params.id || !req.file.filename) throw " error : Incomplete Data received";
+     
+  let userId = req.params.id;
+  let firstName=req.body.firstName;
+  let lastName=req.body.lastName;
+  let profileImage= req.file.filename;
+  if (
+    typeof userId !== 'string' ||
+    typeof firstName !== 'string' ||
+    typeof lastName !== 'string' ||
+    typeof profileImage !== 'string'
+  ) {
+    throw ' not a valid inputs';
+  }
+
+    const updateUser = await users.updateUser(userId,firstName,lastName,profileImage);
+   // updateuser returns the updated user from users collection
+  //  let success = req.file.filename+ "uploaded successfully"
+    res.status(200).render("users/get_specific_user", updateUser);
     return;
   } catch (e) {
     res.status(400).render("users/get_specific_user", { error: e });
