73017bd2d6e259194ccaf6f592425e7e66dcc74f
Aravindh Shiva <64689564+asg-dev@users.noreply.github.com>
2021-12-22 10:41:21 -0500

    Update email.js

--- data/email.js
+++ data/email.js
@@ -6,7 +6,7 @@ const transporter = nodemailer.createTransport({
   secure: false,
   auth: {
     user: "questionnaire_assist@outlook.com",
-    pass: "Fduser@123",
+    pass: "SECURE_PASSWORD",
   },
 });
 
